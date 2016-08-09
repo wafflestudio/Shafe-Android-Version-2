1dea6a1de01c36291ee3f36ae90a8b58c57cf97f
Heesu Suh <hssuh96@gmail.com>
2016-08-09 11:29:55 +0900

    edit layout and add toolbar

--- app/src/main/java/com/wafflestudio/shafe/MainActivity.java
+++ app/src/main/java/com/wafflestudio/shafe/MainActivity.java
@@ -1,7 +1,11 @@
 package com.wafflestudio.shafe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+//                startActivity(new Intent(this, SearchActivity.class));
+                return true;
+            case R.id.action_map:
+//                startActivity(new Intent(this, MapActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
