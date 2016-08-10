c66e5c07ba62d973b8f319548627cc6aa83050b9
Heesu Suh <hssuh96@gmail.com>
2016-08-10 12:59:56 +0900

    add MapView in MapActivity

--- app/src/main/java/com/wafflestudio/shafe/MapActivity.java
+++ app/src/main/java/com/wafflestudio/shafe/MapActivity.java
@@ -2,6 +2,10 @@ package com.wafflestudio.shafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        //set toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(myToolbar);
+        //enable up button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        MapView mapView = new MapView(this);
+        mapView.setDaumMapApiKey(BuildConfig.SHAFE_DAUM_MAP_API_KEY);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
