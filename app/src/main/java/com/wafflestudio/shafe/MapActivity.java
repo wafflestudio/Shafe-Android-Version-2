96b6f9d61c105bc1bfafe6af1b0bcd85df56c9ee
Heesu Suh <hssuh96@gmail.com>
2016-09-18 14:18:35 +0900

    add funtionalities to MapManager and modify MapActivity to load current location

--- app/src/main/java/com/wafflestudio/shafe/MapActivity.java
+++ app/src/main/java/com/wafflestudio/shafe/MapActivity.java
@@ -1,13 +1,33 @@
 package com.wafflestudio.shafe;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 
 import net.daum.mf.map.api.MapView;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener {
+
+    protected static final String TAG = "MainActivity";
+
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+
+    protected MapManager searchResultMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +43,85 @@ public class MapActivity extends AppCompatActivity {
 
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
 
-        MapManager searchResultMap = MapManager.getInstance(this);
+        buildGoogleApiClient();
+
+        searchResultMap = MapManager.getInstance(this);
 
         mapViewContainer.addView(searchResultMap.getMapView());
+    }
+
+    /**
+     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+        // TODO : save를 어느 시점에서 할지 고민 해볼 것
+        searchResultMap.saveMapCenterPointAndZoomLevel();
+    }
+
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: 이거 희준이형이 비슷한 코드 넣었던거 같은데 나중에 머지하고 확인할 것
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            searchResultMap.loadCurrentLocation(mLastLocation.getLatitude(), mLastLocation.getLongitude(), false);
+            searchResultMap.initializeMapWithSavedValue();
+        }
+        else {
+            Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 }
--- app/src/main/java/com/wafflestudio/shafe/MapManager.java
+++ app/src/main/java/com/wafflestudio/shafe/MapManager.java
@@ -4,17 +4,22 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public final class MapManager {
     private static final MapManager instance = new MapManager();
     private static MapView mapView = null;
+    private static MapPoint mapCenterPoint;
+    private static int zoomLevel = 2;
 
     private MapManager() {}
 
     // getInstance는 한 번만 호출할 것
     public static MapManager getInstance(Context context) {
         mapView = new MapView(context);
+
         mapView.setDaumMapApiKey(BuildConfig.SHAFE_DAUM_MAP_API_KEY);
 
         return instance;
@@ -22,5 +27,43 @@ public final class MapManager {
 
     public MapView getMapView() {
         return mapView;
+    }
+
+//    public void setMapCenterPoint(MapPoint centerPoint, boolean animated) {
+//        mapView.setMapCenterPoint(centerPoint, animated);
+//    }
+//
+//    public void setZoomLevel(int zoomLevel, boolean animated) {
+//        mapView.setZoomLevel(zoomLevel, animated);
+//    }
+
+    public void initializeMapWithSavedValue() {
+        // animated를 false로 넣어주면 에러가 뜸. 왜인지는 잘 모르겠음
+        mapView.setMapCenterPointAndZoomLevel(mapCenterPoint, zoomLevel, true);
+    }
+
+    public void loadCurrentLocation(double latitude, double longitude, boolean overrideSavedValue) {
+        if (mapCenterPoint == null || overrideSavedValue) {
+            mapCenterPoint = MapPoint.mapPointWithGeoCoord(latitude, longitude);
+        }
+    }
+
+    public void saveMapCenterPointAndZoomLevel() {
+        mapCenterPoint = mapView.getMapCenterPoint();
+        zoomLevel = mapView.getZoomLevel();
+    }
+
+    // add POI item to mapView
+    public void addPOIItem(MapPOIItem poiItem) {
+        mapView.addPOIItem(poiItem);
+    }
+
+    // add POI items to mapView
+    public void addPOIItems(MapPOIItem[] poiItems) {
+        mapView.addPOIItems(poiItems);
+    }
+
+    public void setPOIItemEventListener(MapView.POIItemEventListener poiItemEventListener) {
+        mapView.setPOIItemEventListener(poiItemEventListener);
     }
 }
